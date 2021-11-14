434bad5ad57386b80303df07b5514bb1dd548639
Diego Rivasplata <drivasplata@gmail.com>
2021-11-14 18:09:53 -0500

    eliminar archivos anteriores

--- src/main/java/com/admin/proyecto/Models/Contratos/IEmpleadoDao.java
+++
@@ -1,5 +0,0 @@
-package com.admin.proyecto.Models.Contratos;
-
-public interface IEmpleadoDao {
-  
-}
--- src/main/java/com/admin/proyecto/Repository/InterfacesJPA/IEmpleadoJPA.java
+++
@@ -1,9 +0,0 @@
-package com.admin.proyecto.Repository.InterfacesJPA;
-
-import com.admin.proyecto.Models.Entities.Empleado;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IEmpleadoJPA extends JpaRepository<Empleado, Long>{
-  
-}
